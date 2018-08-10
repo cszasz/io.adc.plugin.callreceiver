f1308ed674535d3c656637c69a0eeeca9d686cea
Ahmed Chtara <ahmed.chtara@gmail.com>
2018-08-10 11:08:36 +0100

    update

--- README.md
+++ README.md
@@ -1,16 +1,42 @@
-Cordova PhoneCall Receiver
+Cordova AdcCallReceiver
 =======================
 
-It is a Apache Cordova plugin to simplify handling phone call status and events in Android devices.
+It is a Apache Cordova plugin (COMPATIBLE WITH IONIC) to simplify handling phone call status and events in Android devices.
 
 
 ## Install
 
+cordova plugin add https://github.com/ahmedb49/io.adc.plugin.callreceiver
     
 ******
 
-## Quick Example
+## Quick Example (IONIC3 example)
 
+platform.ready().then(() => {
+    ..... 
+	this.initMyPlugin();
+    .....
+});
+initMyPlugin(){
+	if ( (<any>window).AdcCallReceiver){
+      (<any>window).AdcCallReceiver.onCall(function (state: string) {
+	  
+           if (state=="RINGING"){ //INCOMING CALL
+		   .......
+		   }
+		   else if (state=="OFFHOOK"){ //CALL STARTED
+		   .......
+		   }
+		   else if (state=="IDLE"){ //CALL ENDED
+		   .......
+		   }
+          
+          });
+    }
+    else {
+      console.log("plugin not found");
+    }
+}	
     
 *********
 
@@ -20,10 +46,13 @@ It is a Apache Cordova plugin to simplify handling phone call status and events 
 
 
 ## References
+Cordova PhoneCall Trap : worked with PhoneStateListener
+https://github.com/renanoliveira/cordova-phone-call-trap
 
+AdcCallReceiver worked BroadcastReceiver
 ********
 
 
 ## License
 
-Cordova PhoneCall receiver is not released yet
+MIT
--- src/android/AdcCallReceiver.java
+++ src/android/AdcCallReceiver.java
@@ -7,83 +7,49 @@ import org.apache.cordova.PluginResult;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.telephony.TelephonyManager;
-import com.google.gson.Gson;
+
 
 import org.json.JSONException;
 import org.json.JSONArray;
 
-import org.apache.cordova.CordovaWebView;
 
 public class AdcCallReceiver extends CordovaPlugin {
 
 
-	CallReceiver callReceiver;
-    public  CallbackContext callbackContext;
+	
     public static  CallbackContext cbc;
   
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         
-     
-
-        if (callbackContext!=null){
-            Context context=this.cordova.getActivity().getApplicationContext(); 
-            SharedPreferences prefs = context.getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
-            SharedPreferences.Editor prefsEditor = prefs.edit();
-            Gson gson = new Gson();
-            String json = gson.toJson(callbackContext);
-            prefsEditor.putString("callbackContext", json);
-            prefsEditor.commit();
-        }
-
-		if (callReceiver==null){
-			callReceiver= new CallReceiver();
-            callReceiver.setCallbackContext(callbackContext);
-            AdcCallReceiver.cbc=callbackContext;
-            
-		}
-        
+        AdcCallReceiver.cbc=callbackContext;
         return true;
     }
-
-    
+       
 
     public static class CallReceiver extends BroadcastReceiver {
 
-	
-		private CallbackContext callbackContext;
-
-		public void setCallbackContext(CallbackContext callbackContext) {
-			this.callbackContext = callbackContext;
-		}
-        
         @Override
         public void onReceive(Context context, Intent intent) {
            
-        
-            
-             String msg="";
-            if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
-                msg="Call started...";
+            String msg="";
+			if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)){
+                msg="RINGING"; 
+                sendResult(msg);
+            }
+            else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
+                msg="OFFHOOK";
                 sendResult(msg);
                  
             }
             else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_IDLE)){
-              
-              
-                msg="Call ended...";
+                msg="IDLE";
 				sendResult(msg);
             }
-            else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)){
-                
-             
-             
-                msg="Incoming call..."; 
-                sendResult("Incoming call...");
-            }
+            
             
         }
+
 		public void sendResult(String msg){
 			PluginResult result = new PluginResult(PluginResult.Status.OK, msg);
 			result.setKeepCallback(true);
@@ -91,21 +57,15 @@ public class AdcCallReceiver extends CordovaPlugin {
             if (AdcCallReceiver.cbc!=null){
 			AdcCallReceiver.cbc.sendPluginResult(result);
             }
-            else {
-                Gson gson = new Gson();
-                String json = prefs.getString("callbackContext", "");
-                CallbackContext callbackContext = gson.fromJson(json, CallbackContext.class);
-                if (callbackContext!=null){
-                    callbackContext.sendPluginResult(result);
-                }
-                
-            }
             
-		}
+        } 
+            
+            
+	}
 
-    }   
+}   
    
 
-}
+
 
 
