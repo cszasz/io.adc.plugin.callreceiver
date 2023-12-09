930cb8a0019547da5a435a690090c9dd0ae7868f
MacMini <cszasz@gmail.com>
2023-12-09 14:07:32 +0100

    Update AdcCallReceiver.java

--- src/android/AdcCallReceiver.java
+++ src/android/AdcCallReceiver.java
@@ -1,14 +1,20 @@
 package io.adc.adccallreceiver;
 
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.TelephonyManager;
 
+
+import androidx.core.app.ActivityCompat;
 
 import org.json.JSONException;
 import org.json.JSONArray;
@@ -25,7 +31,14 @@ public class AdcCallReceiver extends CordovaPlugin {
         AdcCallReceiver.cbc=callbackContext;
         return true;
     }
-       
+
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+           ActivityCompat.requestPermissions(cordova.getActivity(),
+                        new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_CALL_LOG},
+                        1);
+
+    }
 
     public static class CallReceiver extends BroadcastReceiver {
 
@@ -35,27 +48,33 @@ public class AdcCallReceiver extends CordovaPlugin {
             String msg="";
 			if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)){
                 msg="RINGING"; 
-                sendResult(msg);
+                sendResult(msg, intent);
             }
             else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
                 msg="OFFHOOK";
-                sendResult(msg);
+                sendResult(msg, intent);
                  
             }
             else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_IDLE)){
                 msg="IDLE";
-				sendResult(msg);
+				sendResult(msg, intent);
+            } else {
+                sendResult("x", intent);
             }
+
+
             
             
         }
 
-		public void sendResult(String msg){
-			PluginResult result = new PluginResult(PluginResult.Status.OK, msg);
-			result.setKeepCallback(true);
+		public void sendResult(String msg, Intent intent){
+            if (intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER)!=null) {
+                PluginResult result = new PluginResult(PluginResult.Status.OK, "{ state: \"" + msg + "\", number:\"" + intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER) + "\"}");
+                result.setKeepCallback(true);
 
-            if (AdcCallReceiver.cbc!=null){
-			AdcCallReceiver.cbc.sendPluginResult(result);
+                if (AdcCallReceiver.cbc != null) {
+                    AdcCallReceiver.cbc.sendPluginResult(result);
+                }
             }
             
         } 
@@ -65,7 +84,6 @@ public class AdcCallReceiver extends CordovaPlugin {
 
 }   
    
-
 
 
 
